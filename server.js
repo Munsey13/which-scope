267130e78193cec0aa26fa7c7a229acf940bd322
MM13 <Fastsoccer13@gmail.com>
2020-03-05 20:22:16 -0500

    set up server to be able to run express-handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const exphbs = require("express-handlbars");
 const PORT = 3000;
 
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
     console.log("Application listening on " + PORT);
