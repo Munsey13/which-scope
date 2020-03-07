7953344e45923c8005e3a4cd976a860dba798183
MM13 <Fastsoccer13@gmail.com>
2020-03-07 09:26:18 -0500

    just trying to get my code to function

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const exphbs = require("express-handlbars");
 const PORT = 3000;
+const path = require("path");
+//const routes = require("./routes");
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"}));
-app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+//app.use(routes);
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + ".public/index.html"));
+});
 
 app.listen(PORT, function() {
     console.log("Application listening on " + PORT);
